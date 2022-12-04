d1fd3a13346a284ff9e30fac89b51d0186ae968c
Linh Bui <linh@Linhs-iMac.local>
2022-12-04 13:50:51 -0800

    added search functionality to the API

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
     let myAPIKey = "0946b628138388ac70a5b0e5415fce8c";
-    let city = "Los Angeles";
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + myAPIKey;
-    fetch(url)
-        .then(function (response) {
+    $('.search-button').click(function() {
+        let city = $("#search-box").val();
+        console.log(city)
+        let url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + myAPIKey;
+        fetch(url).then(function (response) {
             return response.json();
+        })
+        .then(function (data){
+            console.log(data);
+        })
     })
-    .then(function (data) {
-    console.log(data);
-	});
 })
+
